1c28a226bf3972f03c2e9849ef3bc3a9e3f9b032
Oleh Haidar <oleh.haidar@gmail.com>
2022-04-08 12:05:41 +0300

    added launch screen, refactoring

---
+++ KidsApp/Views/Home/AddKidView.swift
@@ -0,0 +1,20 @@
+//
+//  AddKidView.swift
+//  KidsApp
+//
+//  Created by Oleh Haidar on 05.04.2022.
+//
+
+import SwiftUI
+
+struct AddKidView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddKidView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddKidView()
+    }
+}
