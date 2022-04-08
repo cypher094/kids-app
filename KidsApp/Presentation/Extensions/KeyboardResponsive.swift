006abf807bcd7cbe74b2eda33ddaae64b1703588
Oleh Haidar <oleh.haidar@gmail.com>
2022-04-08 12:16:29 +0300

    added pattern for card number

--- KidsApp/Presentation/Extensions/KeyboardResponsive.swift
+++ KidsApp/Presentation/Extensions/KeyboardResponsive.swift
@@ -38,3 +38,17 @@ extension View {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
+
+extension String {
+    func applyCardPattern(pattern: String = "#### #### #### ####", replacmentCharacter: Character = "#") -> String {
+        var pureNumber = self.replacingOccurrences( of: "[^0-9]", with: "", options: .regularExpression)
+        for index in 0 ..< pattern.count {
+            guard index < pureNumber.count else { return pureNumber }
+            let stringIndex = String.Index(utf16Offset: index, in: self)
+            let patternCharacter = pattern[stringIndex]
+            guard patternCharacter != replacmentCharacter else { continue }
+            pureNumber.insert(patternCharacter, at: stringIndex)
+        }
+        return pureNumber
+    }
+}
